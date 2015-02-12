56bf5417545cdfeb226e7dda745f3461dab8c0f6
SebCorbin <seb.corbin@gmail.com>
2015-02-12 11:41:18 +0100

    Simplify

--- export_selection_to_svg.jsx
+++ export_selection_to_svg.jsx
@@ -1,71 +1,61 @@
 /*
  * Export selection to SVG - export_selection_as_SVG
- * (Adapted from Layers to SVG 0.1 - layers_export.jsx, by Anton Ball)
+ * (Adapted from Layers to SVG 0.1 - export_selection_as_SVG.jsx, by Rhys van der Waerden)
  *
- * @author Rhys van der Waerden
+ * @author SebCorbin
  */
 
 // Variables
 var ignoreHidden = true,
-    svgExportOptions = (function(){
-        var options = new ExportOptionsSVG();
-        options.fontSubsetting = SVGFontSubsetting.GLYPHSUSED;
-        options.embedRasterImages = false;
-        options.fontType = SVGFontType.OUTLINEFONT;
-        return options;
-    }());
+  svgExportOptions = (function () {
+    var options = new ExportOptionsSVG();
+    options.fontSubsetting = SVGFontSubsetting.GLYPHSUSED;
+    options.embedRasterImages = false;
+    options.fontType = SVGFontType.OUTLINEFONT;
+    return options;
+  }());
 
 // Copy selection to a new document, and save it as an SVG file.
-var saveSelection = function(file){
-    var objects = app.activeDocument.selection;
-    if(objects.length > 0) {
-        // Create temporary document and copy all selected objects.
-        var doc = app.documents.add(DocumentColorSpace.RGB);
-        app.coordinateSystem = CoordinateSystem.ARTBOARDCOORDINATESYSTEM;
-        copyObjectsTo(objects, doc);
-        var offset = doc.visibleBounds;
-        var items = doc.pageItems;
-        var count = items.length;
-        for ( var i = 0; i < count; i++ ) {
-            items[i].translate( -(offset[0]), -(offset[1]) );
-        };
-        doc.artboards[0].artboardRect = doc.visibleBounds;
-        app.redraw();
+var saveSelection = function (file) {
+  var objects = app.activeDocument.selection;
+  if (objects.length > 0) {
+    // Create temporary document and copy all selected objects.
+    var doc = app.documents.add(DocumentColorSpace.RGB);
+    app.coordinateSystem = CoordinateSystem.ARTBOARDCOORDINATESYSTEM;
+    copyObjectsTo(objects, doc);
 
-        // Resize the artboard to the object
-        selectAll(doc);
-        doc.fitArtboardToSelectedArt(0); // Crashes without an index
+    // Resize the artboard to the object
+    selectAll(doc);
+    doc.fitArtboardToSelectedArt(0); // Crashes without an index
 
-        doc.exportFile(file, ExportType.SVG, svgExportOptions);
+    doc.exportFile(file, ExportType.SVG, svgExportOptions);
 
-        // Remove everything
-        doc.activeLayer.pageItems.removeAll();
-        doc.close(SaveOptions.DONOTSAVECHANGES);
-    }
+    // Remove everything
+    doc.activeLayer.pageItems.removeAll();
+    doc.close(SaveOptions.DONOTSAVECHANGES);
+  }
 };
 
 // Duplicate objects and add them to a document.
-var copyObjectsTo = function(objects, destinationDocument){
-    for (var i = 0; i < objects.length; i++) {
-        objects[i].duplicate(
-            destinationDocument.activeLayer,
-            ElementPlacement.PLACEATEND);
-    }
+var copyObjectsTo = function (objects, destinationDocument) {
+  for (var i = 0; i < objects.length; i++) {
+    objects[i].duplicate(destinationDocument.activeLayer, ElementPlacement.PLACEATBEGINNING);
+  }
 };
 
 // Selects all PageItems in the doc
-var selectAll = function(doc){
-    var pageItems = doc.pageItems,
-        numPageItems = doc.pageItems.length;
-    for (var i = 0; i < numPageItems; i += 1){
-        pageItems[i].selected = true;
-    }
+var selectAll = function (doc) {
+  var pageItems = doc.pageItems,
+    numPageItems = doc.pageItems.length;
+  for (var i = 0; i < numPageItems; i += 1) {
+    pageItems[i].selected = true;
+  }
 };
 
 // Init
-(function(){
-    var file = File.saveDialog('Add SVG file', 'SVG:*.svg');
-    if (file) {
-        saveSelection(file);
-    }
+(function () {
+  var file = File.saveDialog('Add SVG file', 'SVG:*.svg');
+  if (file) {
+    saveSelection(file);
+  }
 }());
